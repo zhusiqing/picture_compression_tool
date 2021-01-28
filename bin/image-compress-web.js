1a965d946295cf60f1467fb781678a2566378c39
zhusiqing <zhusiqing@360.cn>
2021-01-28 20:01:44 +0800

    修复环境变量路径问题导致启动失败

--- bin/image-compress-web.js
+++ bin/image-compress-web.js
@@ -1,14 +1,3 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process')
-const path = require('path')
-
-const projectPath = path.resolve(process.argv[1], '../../lib/node_modules/node-images-compress-cli')
-
-const pp = spawn('node', [path.resolve(projectPath, './server/index.js'), '-p 5002'], { stdio: 'inherit' })
-pp.on('error', err => {
-  console.log('error', err)
-})
-pp.on('close', code => {
-  console.log('close')
-})
+require('../server')
